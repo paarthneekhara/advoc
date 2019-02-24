92ae291b6d0f106884174bab7395d3dff712ffc2
Chris Donahue <christopherdonahue@gmail.com>
2019-02-23 22:55:38 -0800

    Tested tacotron2

--- tests/test_spectral.py
+++ tests/test_spectral.py
@@ -24,14 +24,22 @@ class TestSpectralModule(unittest.TestCase):
   def test_stft(self):
     X = spectral.stft(self.wav_mono_44, 1024, 256)
 
+
+  def test_tacotron2(self):
+    melspec = spectral.waveform_to_tacotron2_feats(self.wav_mono_24)
+    self.assertEqual(melspec.shape, (303, 80, 1), 'invalid shape')
+    self.assertAlmostEqual(np.sum(melspec), 5122.212, 3, 'invalid shape')
+    self.assertAlmostEqual(np.sum(melspec[200]), 31.119, 3, 'invalid shape')
+    self.assertAlmostEqual(np.sum(melspec[40]), 5.132, 3, 'invalid shape')
+
+
   def test_r9y9(self):
+    melspec = spectral.waveform_to_r9y9_feats(self.wav_mono_22)
+    self.assertEqual(melspec.shape, (325, 80, 1), 'invalid shape')
+
     with open(WAV_MONO_R9Y9, 'rb') as f:
       r9y9_melspec = pickle.load(f)
     r9y9_melspec = np.swapaxes(r9y9_melspec, 0, 1)[:, :, np.newaxis]
-
-    melspec = spectral.waveform_to_r9y9_feats(self.wav_mono_22)
-
-    self.assertEqual(melspec.shape, (325, 80, 1))
     self.assertTrue(np.array_equal(melspec, r9y9_melspec), 'not equal r9y9')
 
 
