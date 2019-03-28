8f8bdfc9c1ce35a4f9d14e0dd257e582c09982f1
Paarth Neekhara <paarth.n@gmail.com>
2019-03-28 14:15:45 -0700

    made things customizable

--- srezmelspec/melspecVocoder.py
+++ srezmelspec/melspecVocoder.py
@@ -21,6 +21,7 @@ def main():
   parser.add_argument('--heuristic', type=str)
   parser.add_argument('--n_mels', type=int)
   parser.add_argument('--fs', type=int)
+  parser.add_argument('--subseq_len', type=int)
 
   parser.set_defaults( 
     input_file=None,
@@ -29,7 +30,8 @@ def main():
     meta_fp=None,
     heuristic="lws",
     n_mels=80,
-    fs=22050
+    fs=22050,
+    subseq_len = 256
     )
   args = parser.parse_args()
 
@@ -53,10 +55,10 @@ def main():
     _mel_spec = np.load(fp)[:,:,0]
     X_mag = su.tacotron_mel_to_mag(_mel_spec)
     x_mag_original_length = X_mag.shape[0]
-    x_mag_target_length = int(X_mag.shape[0] / 256 ) * 256 + 256
+    x_mag_target_length = int(X_mag.shape[0] / subseq_len ) * subseq_len + subseq_len
     X_mag = np.pad(X_mag, ([0,x_mag_target_length - X_mag.shape[0]], [0,0]), 'constant')
-    num_examples = int(x_mag_target_length/256)
-    X_mag = np.reshape(X_mag, [num_examples, 256, 513, 1])
+    num_examples = int(x_mag_target_length/subseq_len)
+    X_mag = np.reshape(X_mag, [num_examples, subseq_len, 513, 1])
     gen_mags = []
     heuristic_mags = []
     for n in range(num_examples):
@@ -69,6 +71,7 @@ def main():
     heur_mag = np.concatenate(heuristic_mags, axis = 0)
 
     _gen_audio = su.audio_from_mag_spec(gen_mag)
+    gen_mag = gen_mag[0:x_mag_original_length]
 
     if args.heuristic == 'lws':
       _gen_audio = spectral.magspec_to_waveform_lws(gen_mag, 1024, 256)
@@ -77,15 +80,10 @@ def main():
     else:
       raise NotImplementedError()
 
-    _gen_audio = _gen_audio[:x_mag_original_length * 256, :, :]
-    
     fn = fp.split("/")[-1][:-3] + "wav"
     output_file_name = os.path.join(args.output_dir, fn)
     print("Writing", fidx, output_file_name)
-    audioio.save_as_wav(output_file_name, args.fs, _gen_audio)
-    
-
-  
+    audioio.save_as_wav(output_file_name, args.fs, _gen_audio)  
 
 if __name__ == '__main__':
   main()
