8ff6a19e04745958e139cb9ecd01a1b02a6815fa
Chris Donahue <christopherdonahue@gmail.com>
2019-04-04 12:31:24 -0700

    Added srez to models

---
+++ models/advoc/melspecVocoder.py
@@ -0,0 +1,121 @@
+import numpy as np
+import tensorflow as tf
+import lws
+from advoc import audioio
+from advoc import spectral
+from model import Modes
+from srezModel import SrezMelSpec
+from util import override_model_attrs
+from argparse import ArgumentParser
+import spectral_util
+import os
+import glob
+import scipy.io.wavfile
+import time
+
+
+def main():
+  parser = ArgumentParser()
+  parser.add_argument('--input_dir', type=str)
+  parser.add_argument('--output_dir', type=str)
+  parser.add_argument('--meta_fp', type=str)
+  parser.add_argument('--ckpt_fp', type=str)
+  parser.add_argument('--heuristic', type=str)
+  parser.add_argument('--n_mels', type=int)
+  parser.add_argument('--fs', type=int)
+  parser.add_argument('--subseq_len', type=int)
+
+  parser.set_defaults( 
+    input_file=None,
+    output_dir=None,
+    ckpt_fp=None,
+    meta_fp=None,
+    heuristic="lws",
+    n_mels=80,
+    fs=22050,
+    subseq_len = 256
+    )
+  args = parser.parse_args()
+
+  if not os.path.isdir(args.output_dir):
+    os.makedirs(args.output_dir)
+
+  gen_graph = tf.Graph()
+  with gen_graph.as_default():
+    gan_saver = tf.train.import_meta_graph(args.meta_fp)
+
+  gen_sess = tf.Session(graph=gen_graph)
+  print("Restoring")
+  gan_saver.restore(gen_sess, args.ckpt_fp)
+  gen_mag_spec = gen_graph.get_tensor_by_name('generator/decoder_1/strided_slice_1:0')
+  x_mag_input = gen_graph.get_tensor_by_name('ExpandDims_1:0')
+
+  su = spectral_util.SpectralUtil(n_mels = args.n_mels, fs = args.fs)
+
+  spec_fps = glob.glob(os.path.join(args.input_dir, '*.npy'))
+  subseq_len = args.subseq_len
+
+  start = time.time()
+  for fidx, fp in enumerate(spec_fps):
+    _mel_spec = np.load(fp)[:,:,0]
+    X_mag = su.tacotron_mel_to_mag(_mel_spec)
+    x_mag_original_length = X_mag.shape[0]
+    x_mag_target_length = int(X_mag.shape[0] / subseq_len ) * subseq_len + subseq_len
+    X_mag = np.pad(X_mag, ([0,x_mag_target_length - X_mag.shape[0]], [0,0]), 'constant')
+    num_examples = int(x_mag_target_length/subseq_len)
+    X_mag = np.reshape(X_mag, [num_examples, subseq_len, 513, 1])
+    gen_mags = []
+    heuristic_mags = []
+    for n in range(num_examples):
+      _gen, _heur = gen_sess.run([gen_mag_spec, x_mag_input], feed_dict = {
+          x_mag_input : X_mag[n:n+1]
+          })
+      
+      _gen = np.clip(_gen, 0, None)
+
+      gen_mags.append(_gen[0])
+      heuristic_mags.append(_heur[0])
+    gen_mag = np.concatenate(gen_mags, axis = 0)
+    heur_mag = np.concatenate(heuristic_mags, axis = 0)
+
+    _gen_audio = su.audio_from_mag_spec(gen_mag)
+    gen_mag = gen_mag[0:x_mag_original_length]
+
+    if args.heuristic == 'lws':
+      _gen_audio = spectral.magspec_to_waveform_lws(gen_mag.astype('float64'), 1024, 256)
+    elif args.heuristic == 'gl':
+      _gen_audio = spectral.magspec_to_waveform_griffin_lim(gen_mag, 1024, 256)
+    else:
+      raise NotImplementedError()
+
+    fn = fp.split("/")[-1][:-3] + "wav"
+    output_file_name = os.path.join(args.output_dir, fn)
+    print("Writing", fidx, output_file_name)
+    audioio.save_as_wav(output_file_name, args.fs, _gen_audio)  
+  end = time.time()
+  print("Execution Time in Seconds", end - start)
+
+if __name__ == '__main__':
+  main()
+
+
+'''
+export CUDA_VISIBLE_DEVICES="0" 
+python melspecVocoder.py \
+--input_dir /data2/advoc/Clock/ClockSpectrogram \
+--meta_fp /data2/paarth/TrainDir/srezSpec/ljSpeech/defaultWithoutBatchnormBS8/eval_valid/best_gen_loss_l1-100001.meta \
+--ckpt_fp /data2/paarth/TrainDir/srezSpec/ljSpeech/defaultWithoutBatchnormBS8/eval_valid/best_gen_loss_l1-100001 \
+--output_dir /data2/paarth/dump; \
+
+
+export CUDA_VISIBLE_DEVICES="0" 
+python melspecVocoder.py \
+--input_dir /data2/advoc/Clock/ClockSpectrogram \
+--subseq_len 64 \
+--meta_fp /data2/paarth/TrainDir/srezSpec/ljSpeech/small/encLayers4/eval_valid/best_gen_loss_l1-50001.meta \
+--ckpt_fp /data2/paarth/TrainDir/srezSpec/ljSpeech/small/encLayers4/eval_valid/best_gen_loss_l1-50001 \
+--output_dir /data2/paarth/dump; \
+
+
+
+'''
---
+++ models/advoc/spectral_util.py
@@ -0,0 +1,60 @@
+import advoc
+import tensorflow as tf
+import lws
+import numpy as np
+
+class SpectralUtil(object):
+  NFFT = 1024
+  NHOP = 256
+  FMIN = 125.
+  FMAX = 7600.
+  NMELS = 80
+  fs = 22050
+
+  def __init__(self, n_mels = 80, fs = 22050):
+    self.NMELS = n_mels
+    self.fs = fs
+    meltrans = advoc.spectral.create_mel_filterbank(
+            self.fs, self.NFFT, fmin=self.FMIN, fmax=self.FMAX, n_mels=self.NMELS)
+    invmeltrans = advoc.spectral.create_inverse_mel_filterbank(
+            self.fs, self.NFFT, fmin=self.FMIN, fmax=self.FMAX, n_mels=self.NMELS)
+
+    self.invmeltrans_np = invmeltrans
+    self.meltrans_np = meltrans
+
+    self.meltrans = tf.constant(meltrans, dtype = 'float32')
+    self.invmeltrans = tf.constant(invmeltrans, dtype = 'float32')
+    self.lws_processor = lws.lws(self.NFFT, self.NHOP, mode='speech', perfectrec=False)
+
+  def mag_to_mel_linear_spec(self, mag_spec):
+    linear_mel =  tf.expand_dims(
+      tf.tensordot(mag_spec[:,:,:,0], tf.transpose(self.meltrans), axes = 1 ), -1)
+    return linear_mel
+
+  def mel_linear_to_mag_spec(self, mel_spec, transform = 'inverse'):
+    if transform == 'inverse':
+      transform_mat = tf.transpose(self.invmeltrans)
+    elif transform == 'transposed':
+      transform_mat = meltrans
+    else:
+      raise NotImplementedError()
+    mag_spec =  tf.expand_dims(
+      tf.tensordot(mel_spec[:,:,:,0], transform_mat, axes = 1 ), -1)
+    return mag_spec
+
+  def audio_from_mag_spec(self, mag_spec):
+    mag_spec = mag_spec.astype('float64')
+    spec_lws = self.lws_processor.run_lws(mag_spec[:,:,0])
+    magspec_inv = self.lws_processor.istft(spec_lws)[:, np.newaxis, np.newaxis]
+    magspec_inv = magspec_inv.astype('float32')
+    return magspec_inv
+
+  def tacotron_mel_to_mag(self, X_mel_dbnorm):
+    norm_min_level_db = -100
+    norm_ref_level_db = 20
+    fs = self.fs
+    
+    X_mel_db = (X_mel_dbnorm * -norm_min_level_db) + norm_min_level_db
+    X_mel = np.power(10, (X_mel_db + norm_ref_level_db) / 20)
+    X_mag = np.dot(X_mel, self.invmeltrans_np.T)
+    return X_mag
